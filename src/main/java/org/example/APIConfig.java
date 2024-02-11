3816d413c511fa7267e73105b4885efc7a44726c
FernandoPerezDeAyala <fernandopdab@gmail.com>
2024-02-11 21:04:07 +0100

    Codigo consultas

---
+++ src/main/java/org/example/APIConfig.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class APIConfig {
+
+    /**
+     * Clave de la API utilizada para autenticar las solicitudes a la API externa.
+     */
+    public static final String API_KEY = "3586ea39a8msh8f7e045a940aae8p1e180fjsn725ce591ccc7";
+
+}
